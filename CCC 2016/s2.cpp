2e58b758c0379f40faaa57ae6846fbb27bdb556e
Aaryan Tiwari <sparklingaryan@gmail.com>
2023-01-09 20:48:28 -0500

    Finished 2016 S2

--- CCC 2016/s2.cpp
+++ CCC 2016/s2.cpp
@@ -17,7 +17,15 @@ int main(){
 
     int sum = 0;
 
-    
+    for(int i = 0; i < n; i++){
+        char s;
+        int num;
+        cin >> s >> num;
+        if(jerseys[num-1] <= s){
+            jerseys[num-1] = 'Z';
+            sum += 1;
+        }
+    }
 
     cout << sum;
 
