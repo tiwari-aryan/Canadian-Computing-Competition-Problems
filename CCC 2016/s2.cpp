83bea43502e7dad9cef850670712238498646fcb
Aaryan Tiwari <sparklingaryan@gmail.com>
2023-01-09 20:43:54 -0500

    Finished 2016 S2

---
+++ CCC 2016/s2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector <char> jerseys;
+
+int main(){
+    int j, n;
+    cin >> j >> n;
+    for(int i = 1; i <= j; i++){
+        char jersey;
+        cin >> jersey;
+        jerseys.push_back(jersey);
+    }
+
+    int sum = 0;
+
+    
+
+    cout << sum;
+
+    return 0;
+}
